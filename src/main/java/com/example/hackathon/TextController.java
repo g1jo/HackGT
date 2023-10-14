bc616118dcd6feb1bec3fb7894697cd7d78ddda8
AmirKSalah <57016382+AmirKSalah@users.noreply.github.com>
2023-10-13 23:47:09 -0400

    changed application properties

--- src/main/java/com/example/hackathon/TextController.java
+++ src/main/java/com/example/hackathon/TextController.java
@@ -1,21 +1,18 @@
 package com.example.hackathon;
-
+import com.example.hackathon.Clothing.*;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class TextController {
-
-
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
+    Shirt item = new Shirt(1, 70, "red");
 
-    @GetMapping("/greeting")
+    @GetMapping
     public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
-        return String.format(template, name);
+        return item.getColor();
 }
 }
