f401ad3d62d5ec71675b9e6c57cdcf02650b4bb9
AmirKSalah <57016382+AmirKSalah@users.noreply.github.com>
2023-10-13 21:14:51 -0400

    spring

---
+++ src/main/java/com/example/hackathon/TextController.java
@@ -0,0 +1,21 @@
+package com.example.hackathon;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class TextController {
+
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/greeting")
+    public String greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format(template, name);
+}
+}
