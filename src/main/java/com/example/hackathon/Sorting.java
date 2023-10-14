463f487c839ee2c40b20851c8b76df191139d32c
g1jo <g1garrettjones@gmail.com>
2023-10-14 03:24:18 -0400

    Updating sorting class w/ merge sort

--- src/main/java/com/example/hackathon/Sorting.java
+++ src/main/java/com/example/hackathon/Sorting.java
@@ -2,27 +2,94 @@ package com.example.hackathon;
 import java.util.*;
 
 public class Sorting {
-    private ArrayList<Clothing> sorted = new ArrayList<Clothing>();
+    private int[] desired;
+
     /**
-     * Creates a sorting object using Singleton design
+     * Creates a sorting object
+     * @param desired clothes
      */
-    private static Sorting sorting;
-    private Sorting() {}
-    public static Sorting getSorting() {
-        if (sorting == null) {
-            sorting = new Sorting();
-        }
-        return sorting;
+    public Sorting(int[] desired) {
+        this.desired = desired;
     }
 
+    /**
+     * Merge sort for array of clothing values
+     * @param arr of clothes
+     * @return sorted array of Clothing values
+     */
+    public Clothing[] mergeSort(Clothing[] arr) {
+        if (arr.length < 2) {
+            Clothing[] ex = new Clothing[1];
+            return ex;
+        }
+        Clothing[] leftSub = new Clothing[arr.length / 2];
+        for (int i = 0; i < leftSub.length; i++) {
+            leftSub[i] = arr[i];
+        }
+        Clothing[] rightSub = new Clothing[arr.length - leftSub.length];
+        for (int i = 0; i < rightSub.length; i++) {
+            rightSub[i] = arr[i + leftSub.length];
+        }
+        mergeSort(leftSub);
+        mergeSort(rightSub);
+        return merge(arr, leftSub, rightSub);
+    }
+
+    /**
+     * Helper for merge sort
+     * @param arr to be sorted
+     * @param leftSub part of array
+     * @param rightSub part of array
+     * @return final sorted array
+     */
+    private Clothing[] merge(Clothing[] arr, Clothing[] leftSub, Clothing[] rightSub) {
+        int i = 0, j = 0, k = 0;
+        while (i < leftSub.length && j < rightSub.length) {
+            if (compare(leftSub[i], rightSub[j]) <= 0) {
+                arr[k] = leftSub[i];
+                i++;
+            } else {
+                arr[k] = rightSub[j];
+                j++;
+            }
+            k++;
+        }
+        while (i < leftSub.length) {
+            arr[k] = leftSub[i];
+            i++;
+            k++;
+        }
+        while (j < rightSub.length) {
+            arr[k] = rightSub[j];
+            j++;
+            k++;
+        }
+        return arr;
+    }
+
+    /**
+     * Compares two clothing items
+     * @param cloth1 to be compared
+     * @param cloth2 to be compared
+     * @return positive if cloth1 greater, negative if cloth2 greater, 0 if =
+     */
+    private int compare(Clothing cloth1, Clothing cloth2) {
+    int[] cloth1Arr = toArr(cloth1);
+    int[] cloth2Arr = toArr(cloth2);
+    int[] diff1 = diffFromDesired(desired, cloth1Arr);
+    int[] diff2 = diffFromDesired(desired, cloth2Arr);
+    int sum1 = sum(diff1);
+    int sum2 = sum(diff2);
+    return sum1 - sum2;
+    }
 
     /**
      * Finds how much an item differs from wanted item
-     * @param desired
-     * @param item
+     * @param desired item
+     * @param item current item
      * @return array of difference from desired
      */
-    public int[] diffFromDesired(int[] desired, int[] item) {
+    private int[] diffFromDesired(int[] desired, int[] item) {
         int[] diff = new int[desired.length];
         for (int i = 0; i < desired.length; i++) {
             diff[i] = desired[i] - item[i];
@@ -31,25 +98,11 @@ public class Sorting {
     }
 
     /**
-     * Compares two clothing items
-     * @param cloth1
-     * @param cloth2
-     * @return positive if cloth1 greater, negative if cloth2 greater, 0 if =
-     */
-    private int compareClothes(Clothing cloth1, Clothing cloth2) {
-        int[] cloth1Arr = toVector(cloth1);
-        int[] cloth2Arr = toVector(cloth2);
-        int sum1 = sum(cloth1Arr);
-        int sum2 = sum(cloth2Arr);
-        return sum1 - sum2;
-    }
-
-    /**
      * Converts clothing object to an array
-     * @param clothing
+     * @param clothing to be converted to array
      * @return array representing clothing object
      */
-    private int[] toVector(Clothing clothing) {
+    private int[] toArr(Clothing clothing) {
         int[] arr = new int[4];
         arr[0] = clothing.rain() ? 1 : 0;
         arr[1] = clothing.getStyleRange();
@@ -60,7 +113,7 @@ public class Sorting {
 
     /**
      * Convert color to ints
-     * @param color
+     * @param color of item
      * @return int for each color
      */
     private int toInt(String color) {
@@ -93,7 +146,7 @@ public class Sorting {
 
     /**
      * Finds the sum of the array
-     * @param arr
+     * @param arr to be summed
      * @return sum of an array
      */
     private int sum(int[] arr) {
@@ -102,5 +155,22 @@ public class Sorting {
             sum += arr[i];
         }
         return sum;
+    }
+
+    /**
+     * Combine two arrays of clothes
+     * @param arr1 first array to be combined
+     * @param arr2 second array to be combined
+     * @return combined array of clothes
+     */
+    private Clothing[] concat(Clothing[] arr1, Clothing[] arr2) {
+        Clothing[] arr = new Clothing[arr1.length + arr2.length];
+        for (int i = 0; i < arr1.length; i++) {
+            arr[i] = arr1[i];
+        }
+        for (int i = 0; i < arr2.length; i++) {
+            arr[i + arr1.length] = arr2[i];
+        }
+        return arr;
     }
 }
